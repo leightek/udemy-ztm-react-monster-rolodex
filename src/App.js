098649692414d8ca00c389c975716b46441c0626
Leigh Su <slhcan@yahoo.com>
2023-05-06 00:31:47 -0700

    [ReactBasic] Updated name to variable in App class

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Leigh',
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi Leigh
-          </p>
-          <button>
-            Change Name
-          </button>
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
