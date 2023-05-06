8313ea984bc727076fc66325c6725bbab785868c
Leigh Su <slhcan@yahoo.com>
2023-05-06 00:23:32 -0700

    [ReactBasic] Updated content in App class

--- src/App.js
+++ src/App.js
@@ -11,16 +11,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi Leigh
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
