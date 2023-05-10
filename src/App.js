9101930bfde4bef35e2d215b937e0081290f578a
Leigh Su <slhcan@yahoo.com>
2023-05-09 22:25:33 -0700

    [ReactBasic] Updated setState with callback functions

--- src/App.js
+++ src/App.js
@@ -21,10 +21,22 @@ class App extends Component {
           <p>
             Hi {this.state.name.firstName} {this.state.name.lastName}, I work 
             at {this.state.company}</p>
-          <button onClick={() => {
-            this.setState({name: {firstName: 'John', lastName: 'Doe'}});
-            console.log(this.state)
-          }}>Change Name</button>
+          <button 
+            onClick={() => {
+              this.setState(() => {
+                return {
+                  name: {firstName: 'John', lastName: 'Doe'},
+                }
+
+                }, 
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
