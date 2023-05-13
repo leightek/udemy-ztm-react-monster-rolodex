70f93c19bd7085e6140843a02e7f998418145c10
Leigh Su <slhcan@yahoo.com>
2023-05-13 00:31:56 -0700

    [ReactBasic] Updated card-list component content

--- src/App.js
+++ src/App.js
@@ -59,14 +59,14 @@ class App extends Component {
           placeholder='search monsters' 
           onChange={onSearchChange} 
         />
-        {
-          /* filteredMonsters.map((monster) => {
+        {/* {
+          filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
               </div>
             );
-          }) */}
+          })} */}
         <CardList />
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,16 @@ import { Component } from 'react';
 class CardList extends Component {
 
     render() {
-      return <div>Hello I'm the CardList Component></div>;
+        return (
+            <div>
+                <h1>Hello I'm the CardList Component</h1>
+                <div>
+                    <p>text</p>
+                </div>
+
+                <div></div>
+            </div>
+        )
     }
 }
 
