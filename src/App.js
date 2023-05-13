6a06c64028033ec8ab65ae05786395225c632c38
Leigh Su <slhcan@yahoo.com>
2023-05-13 01:05:23 -0700

    [ReactBasic] Used monsters as CardList prop

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
               </div>
             );
           })} */}
-        <CardList monsters='I am the monsters' anything={['a', 'z']} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
