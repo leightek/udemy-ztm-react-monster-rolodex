1d1efb4b5eff8a0049ad64e8d056e00e88934ba9
Leigh Su <slhcan@yahoo.com>
2023-05-14 00:34:32 -0700

    [ReactBasic] Added css with demoing impact from individual styling

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
 
+import './search-box.styles.css'; // styling applied to not only this component, but all components
+
 class SearchBox extends Component {
     render() {
         return (
             <input 
-                className={this.props.className} 
+                className={`search-box ${this.props.className}`}
                 type='search' // show X in search box and clear search text when selecting it
                 placeholder={this.props.placeholder} 
                 onChange={this.props.onChangeHandler} 
