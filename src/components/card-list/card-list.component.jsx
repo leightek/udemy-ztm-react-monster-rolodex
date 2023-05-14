e3844deb0ff72ef2e7670cdc1e42dd44fe7845fd
Leigh Su <slhcan@yahoo.com>
2023-05-14 01:24:04 -0700

    [ReactBasic] Optimized CardList using destructing

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -9,16 +9,19 @@ class CardList extends Component {
         
         return (
             <div className='card-list'>
-                {monsters.map((monster) => (
-                    <div className='card-container' key={monster.div} >
-                        <img 
-                            alt={`monster ${monster.name}`} 
-                            src={`https://robohash.org/${monster.id}3?set=set2&size=180x180`}
-                        />
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-                    </div>
-                ))}
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+                    return (
+                        <div className='card-container' key={id} >
+                            <img 
+                                alt={`monster ${name}`} 
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                            />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
