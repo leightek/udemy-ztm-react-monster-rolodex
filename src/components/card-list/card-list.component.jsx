8df84da37a2d273e4df5cb57c86811f827724fc8
Leigh Su <slhcan@yahoo.com>
2023-05-14 01:05:26 -0700

    [ReactBasic] Update CardList with monster data

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -10,7 +10,14 @@ class CardList extends Component {
         return (
             <div className='card-list'>
                 {monsters.map((monster) => (
-                    <h1 key={monster.id}>{monster.name}</h1>
+                    <div className='card-container' key={monster.div} >
+                        <img 
+                            alt={`monster ${monster.name}`} 
+                            src={`https://robohash.org/${monster.id}3?set=set2&size=180x180`}
+                        />
+                        <h2>{monster.name}</h2>
+                        <p>{monster.email}</p>
+                    </div>
                 ))}
             </div>
         );
