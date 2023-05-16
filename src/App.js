d45da5d59c49831ec0c1f637325fddc0f82bb927
Leigh Su <slhcan@yahoo.com>
2023-05-15 19:41:53 -0700

    [ReactBasic] Update App to be functional component with title only

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component'
 import './App.css';
 
-class App extends Component {
+const App = () => {
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      {/* <SearchBox 
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange} 
+        placeholder='search monsters' 
+      />
+  <CardList monsters={filteredMonsters} /> */}
+    </div>
+  )
+}
+
+/* class App extends Component {
   constructor() {
     super();
 
@@ -60,6 +75,6 @@ class App extends Component {
       </div>
     );
   }
-}
+} */
 
 export default App;
