b0a2b4ea7f6e1dc8ab2a069dece74cd94fec7a99
Leigh Su <slhcan@yahoo.com>
2023-05-18 00:17:21 -0700

    [ReactBasic] Updated SearchBox to be functional

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,12 @@
-import { Component } from 'react';
-
 import './search-box.styles.css'; // styling applied to not only this component, but all components
 
-class SearchBox extends Component {
-    render() {
-        return (
-            <input 
-                className={`search-box ${this.props.className}`}
-                type='search' // show X in search box and clear search text when selecting it
-                placeholder={this.props.placeholder} 
-                onChange={this.props.onChangeHandler} 
-            />
-        )
-    }
-}
+const SearchBox = ({ className, placeholder,onChangeHandler }) => (
+    <input 
+        className={`search-box ${className}`}
+        type='search' // show X in search box and clear search text when selecting it
+        placeholder={placeholder} 
+        onChange={onChangeHandler} 
+    />
+);
 
 export default SearchBox;
